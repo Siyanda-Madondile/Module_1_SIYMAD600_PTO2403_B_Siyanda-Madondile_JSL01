34c39d70b062a5529ba7231bfa2e24e65fb6dd5d
Siyanda-Madondile <164426031+Siyanda-Madondile@users.noreply.github.com>
2024-07-24 08:13:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+    let input = document.getElementById('petInput').value; //here we fetch the input result
+    // using a regular expression to define what the format should be of the input field when it is valid
+    let sequence = /^pet_\d{4}[a-zA-Z]+$/; // the only problem here is that the d{4} takes any 4 digits and not strickly dates
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
-
-            document.getElementById('result').innerText = result;
+    let validation; // here a new variable is defined so we can display the results in the innertext
+    // if statement to test if input follows the sequence defined in the regular expression above
+    if (sequence.test(input)) {
+        validation = "Valid Syntax \ud83d\udfe2" //using unicode to add emojis
+    } else {
+        validation = "Invalid Syntax \ud83d\udd34"
+    }
+    document.getElementById('result').innerText = `${validation}`; //here we inject the result into the DOM to display for users
 }
 
 
